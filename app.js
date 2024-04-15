7c5334054bed5c8b5cc7dd0e2a1cd136b03328b0
Krystof Jezdik <jezdik.kr.2021@skola.ssps.cz>
2024-04-15 12:07:23 +0200

    feat: Backend for Blockposts
    Základní backend pro blocpost

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 import helmet from 'helmet';
 import authRouter from './routes/auth/auth.js';
+import blogPostRouter from './routes/auth/blogPost.js'
 import cookieParser from 'cookie-parser';
 
-const port = process.env.PORT;
+const port = 3000;
 
 const app = express();
 
@@ -19,6 +20,7 @@ app.use(cors({
 }));
 
 app.use('/auth', authRouter);
+app.use('/blogPost', blogPostRouter);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
